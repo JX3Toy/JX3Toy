b2adc4050dded615b4668e525bd4f64dd4c32eba
JX3Toy <116929546+JX3Toy@users.noreply.github.com>
2023-04-08 10:16:30 +0800

    Add files via upload

--- 宏/少林/PVE_易筋经.lua
+++ 宏/少林/PVE_易筋经.lua
@@ -1,38 +1,70 @@
---气点同步标志
-local bWait = false
+output("奇穴: [明法][幻身][身意][缩地][降魔渡厄][金刚怒目][净果][三生][众嗔][华香][佛果][业因]")
+
+--变量表
+local v = {}
+v["等待气点同步"] = false
+
 
 --主循环
 function Main(g_player)
-	if nobuff("伏魔") then
-		cast("二业依缘")
-	end
 
 	--等待气点同步
-	if bWait then
-		if gettimer("擒龙诀") < 0.3 or gettimer("罗汉金身") < 0.3 then
+	if v["等待气点同步"] and gettimer("等待气点同步") < 0.5 then
+		return
+	end
+
+	--目标不是敌对结束
+	if not rela("敌对") then return end
+
+	--打断
+	if tbuffstate("可打断") then
+		cast("抢珠式")
+	end
+
+	v["禅那"] = qidian()
+
+	--罗汉金身
+	if rela("敌对") and dis() < 4 and v["禅那"] >= 3 then
+		if cast("罗汉金身") then
+			settimer("等待气点同步")
+			v["等待气点同步"] = true
 			return
 		end
 	end
 
-	if rela("敌对") and dis() < 4 then
-		if qidian() >= 3 then
-			if cast("罗汉金身") then
-				settimer("罗汉金身")
-				bWait = true
-				return
+	--二业依缘
+	if dis() < 4 and cdleft(16) > 0.5 then
+		if nobuff("袈裟") then
+			--袈裟
+			if cn("守缺式") > 0 and tbufftime("横扫六合", id()) > 6 then
+				cast("二业依缘")
 			end
-		end
-		if qidian() < 1 and bufftime("罗汉金身") > 15 then
-			if cast("擒龙诀", true) then
-				settimer("擒龙诀")
-				bWait = true
-				return
+		else
+			--伏魔
+			if nobuff("24620") then		--业因四次无CD
+				if cast("二业依缘") then
+					v["等待气点同步"] = true
+					settimer("等待气点同步")
+				end
 			end
 		end
 	end
 
-	
-	if rela("敌对") and dis() < 5.5 and tbufftime("横扫六合", true) < 2 then
+	--擒龙诀
+	if dis() < 4 and buffsn("果报") >= 3 and v["禅那"] < 2 then
+		cast("擒龙诀")
+	end
+
+	--业因四次无CD
+	if buff("24620") then
+		if qidian() >= 3 then
+			cast("拿云式")
+		end
+		cast("守缺式")
+		return
+	end
+
+	if rela("敌对") and dis() < 5.5 and tbufftime("横扫六合", id()) < 2 then
 		cast("横扫六合")
 	end
 
@@ -41,8 +73,10 @@ function Main(g_player)
 		cast("韦陀献杵")
 	end
 
-	if rela("敌对") and dis() < 12 and qidian() < 3 and bufftime("罗汉金身") > 0.3 then
-		acast("醍醐灌顶")
+	cast("守缺式")
+
+	if rela("敌对") and dis() < 5.5 then
+		cast("横扫六合")
 	end
 
 	cast("守缺式")
@@ -52,6 +86,7 @@ function Main(g_player)
 		cast("捕风式")
 	end
 
+	--捉影式, 插入GCD回豆
 	if cdleft(16) > 0.6 and qidian() < 2 then
 		cast("捉影式")
 	end
@@ -65,5 +100,5 @@ end
 
 --气点更新
 OnQidianUpdate = function()
-	bWait = false
+	v["等待气点同步"] = false
 end
