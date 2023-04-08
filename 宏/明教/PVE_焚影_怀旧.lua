dfe195c081be9eaa3b6c866c38777d47c674f9d1
JX3Toy <116929546+JX3Toy@users.noreply.github.com>
2023-04-08 21:51:02 +0800

    Add files via upload

--- 宏/明教/PVE_焚影_怀旧.lua
+++ 宏/明教/PVE_焚影_怀旧.lua
@@ -1,17 +1,26 @@
+output("----------镇派----------")
+output("2 3 2")
+output("2 2 0 1")
+output("1 3 0 2")
+output("1 2 3 0")
+output("3 2 1")
+output("1")
+output("0 3 2")
+
 --主循环
 function Main(g_player)
 	cast("净世破魔击")
 	
+	if rela("敌对") and dis() < 4 and cdtime("驱夜断愁") > 4 and gettimer("生灭予夺") > 2 then
+		if cast("光明相") then
+			settimer("光明相")
+		end
+	end
+
 	if tface() > 90 or buff("50986|51004") then
-		if rela("敌对") and dis() < 4 and cdtime("驱夜断愁") > 4 then
-			if gettimer("光明相") > 1 and gettimer("生灭予夺") > 1 then
-				if cast("光明相") then
-					settimer("光明相")
-				end
-				
-				if cast("生灭予夺") then
-					settimer("生灭予夺")
-				end
+		if rela("敌对") and dis() < 4 and cdtime("驱夜断愁") > 4 and gettimer("光明相") > 2 then	
+			if cast("生灭予夺") then
+				settimer("生灭予夺")
 			end
 		end
 
